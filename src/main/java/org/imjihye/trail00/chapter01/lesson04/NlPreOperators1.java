95c03576fec97f4ae5b9dee5690e0d900b563ad8
임지혜 <bakcy9842@naver.com>
2025-05-25 14:28:26 +0900

    NlPreOperators1

---
+++ src/main/java/org/imjihye/trail00/chapter01/lesson04/NlPreOperators1.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail00.chapter01.lesson04;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-operators-1/description">LINK</a>
+ */
+public class NlPreOperators1 {
+
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+    System.out.println(c / 8);
+    System.out.println(c % 8);
+  }
+}
