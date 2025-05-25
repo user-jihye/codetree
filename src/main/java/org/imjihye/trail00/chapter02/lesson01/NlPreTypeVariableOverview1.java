a97b789941ad5e8b6c2243bb3f27d602e5121889
임지혜 <bakcy9842@naver.com>
2025-05-25 14:00:45 +0900

    NlPreTypeVariableOverview1

---
+++ src/main/java/org/imjihye/trail00/chapter02/lesson01/NlPreTypeVariableOverview1.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail00.chapter02.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-type-variable-overview-1/description">LINK</a>
+ */
+public class NlPreTypeVariableOverview1 {
+
+  public static void main(String[] args) {
+    char a = 'C';
+    a = 'T';
+    System.out.println(a);
+  }
+}
