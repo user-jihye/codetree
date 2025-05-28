262b633ac34bccb9ce3c03b3903dfd00b49ab803
임지혜 <bakcy9842@naver.com>
2025-05-28 09:59:33 +0900

    NlPreOutput3

---
+++ src/main/java/org/imjihye/trail00/chapter02/lesson01/NlPreOutput3.java
@@ -0,0 +1,15 @@
+package org.imjihye.trail00.chapter02.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-output-3/description">LINK</a>
+ */
+public class NlPreOutput3 {
+
+  public static void main(String[] args) {
+    int weight = 13;
+    double moon = 0.165;
+
+    System.out.printf(weight + " * %.6f = %.6f", moon, weight*moon);
+    
+  }
+}
