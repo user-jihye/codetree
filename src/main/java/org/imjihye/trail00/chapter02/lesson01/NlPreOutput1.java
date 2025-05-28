0c6d4ddb4802bfd0b34a068bbd3958a5cb39852d
임지혜 <bakcy9842@naver.com>
2025-05-28 09:48:49 +0900

    NlPreOutput1

---
+++ src/main/java/org/imjihye/trail00/chapter02/lesson01/NlPreOutput1.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail00.chapter02.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-output-1/description">LINK</a>
+ */
+public class NlPreOutput1 {
+
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    System.out.println(a + "->" + b + "->" + c);
+    
+  }
+}
