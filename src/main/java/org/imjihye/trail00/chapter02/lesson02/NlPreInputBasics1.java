a261222100a66747851aa146f19f98df89f3efcd
임지혜 <bakcy9842@naver.com>
2025-05-30 02:05:20 +0900

    NlPreInputBasics1

---
+++ src/main/java/org/imjihye/trail00/chapter02/lesson02/NlPreInputBasics1.java
@@ -0,0 +1,15 @@
+package org.imjihye.trail00.chapter02.lesson02;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-input-basics-1/description">LINK</a>
+ */
+public class NlPreInputBasics1 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    System.out.println("Your score is " + N + " point.");
+  }
+}
