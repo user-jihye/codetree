2cb43cafc3fdc2ee6d7f1f76e74f9466d9785178
임지혜 <bakcy9842@naver.com>
2025-06-02 10:05:11 +0900

    NlPreInput3

---
+++ src/main/java/org/imjihye/trail00/chapter02/lesson03/NlPreInput3.java
@@ -0,0 +1,18 @@
+package org.imjihye.trail00.chapter02.lesson03;
+
+import java.util.Scanner;
+
+/**import java.util.Scanner;
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-input-3/description">LINK</a>
+ */
+public class NlPreInput3 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + " " + b + " " + (a+b));
+  }
+}
