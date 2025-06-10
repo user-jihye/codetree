d8041ff4cf07b29a378035ca050f275e8bacdc34
임지혜 <bakcy9842@naver.com>
2025-06-10 16:18:21 +0900

    NlPreLoopBasics2

---
+++ src/main/java/org/imjihye/trail00/chapter04/lesson01/NlPreLoopBasics2.java
@@ -0,0 +1,19 @@
+package org.imjihye.trail00.chapter04.lesson01;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-loop-basics-2/description">LINK</a>
+ */
+public class NlPreLoopBasics2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int cnt = sc.nextInt();
+
+    for (int i = 0; i < cnt; i++) {
+      System.out.println("LeebrosCode");
+    }
+
+  }
+}
