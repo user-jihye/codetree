d07c02fd88ca3630e3e0751dea65149ce2f7a354
임지혜 <bakcy9842@naver.com>
2025-06-10 18:33:57 +0900

    NlPreFor1

---
+++ src/main/java/org/imjihye/trail00/chapter04/lesson02/NlPreFor1.java
@@ -0,0 +1,15 @@
+package org.imjihye.trail00.chapter04.lesson02;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-for-1/description">LINK</a>
+ */
+public class NlPreFor1 {
+
+  public static void main(String[] args) {
+
+    for (int i = 5; i <= 17; i++) {
+      System.out.print(i + " ");
+    }
+    
+  }
+}
