7bda84230e08d9cd50950ab9e636c21a170b8b7a
임지혜 <bakcy9842@naver.com>
2025-06-10 15:39:34 +0900

    NlPreLoopBasics1

---
+++ src/main/java/org/imjihye/trail00/chapter04/lesson01/NlPreLoopBasics1.java
@@ -0,0 +1,20 @@
+package org.imjihye.trail00.chapter04.lesson01;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-loop-basics-1/description">LINK</a>
+ */
+public class NlPreLoopBasics1 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    for (int i = 0; i < 8; i++) {
+      System.out.print(str);
+    }
+    
+  }
+}
