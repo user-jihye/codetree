204190d05e15ae903bfaa24d53ecc6ba465ebea0
임지혜 <bakcy9842@naver.com>
2025-06-15 21:23:06 +0900

    NlPre1dArrayBasics1

---
+++ src/main/java/org/imjihye/trail00/chapter06/lesson01/NlPre1dArrayBasics1.java
@@ -0,0 +1,14 @@
+package org.imjihye.trail00.chapter06.lesson01;
+
+import java.util.Arrays;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-1d-array-basics-1/description">LINK</a>
+ */
+public class NlPre1dArrayBasics1 {
+
+  public static void main(String[] args) {
+    int[] numbers = {3, 1, 4, 5, 9};
+    System.out.println(numbers[1] + numbers[3] + numbers[4]);
+  }
+}
