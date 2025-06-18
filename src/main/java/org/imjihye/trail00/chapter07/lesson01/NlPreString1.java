b628df5e9538861be64b6af79b704f10820d5c29
임지혜 <bakcy9842@naver.com>
2025-06-18 09:59:52 +0900

    NlPreString1

---
+++ src/main/java/org/imjihye/trail00/chapter07/lesson01/NlPreString1.java
@@ -0,0 +1,11 @@
+package org.imjihye.trail00.chapter07.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-string-1/description">LINK</a>
+ */
+public class NlPreString1 {
+
+  public static void main(String[] args) {
+    System.out.println("He says \"It's a really simple sentence\".");
+  }
+}
