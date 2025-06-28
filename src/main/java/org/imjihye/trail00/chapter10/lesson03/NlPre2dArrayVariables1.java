4aee370519c230bfc39c385ba0ad4a6e6da08298
임지혜 <bakcy9842@naver.com>
2025-06-28 16:50:14 +0900

    NlPre2dArrayVariables1

---
+++ src/main/java/org/imjihye/trail00/chapter10/lesson03/NlPre2dArrayVariables1.java
@@ -0,0 +1,21 @@
+package org.imjihye.trail00.chapter10.lesson03;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/nl-pre-2d-array-variables-1/description">LINK</a>
+ */
+public class NlPre2dArrayVariables1 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    for (int i = 0; i < 4; i++) {
+      int total = 0;
+      for (int j = 0; j < 4; j++) {
+        total += sc.nextInt();
+      }
+      System.out.println(total);
+    }
+  }
+}
