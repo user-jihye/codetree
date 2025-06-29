541919927e729cc3a17ecb9df3f25b79e690250b
임지혜 <bakcy9842@naver.com>
2025-06-29 16:17:19 +0900

    ChallengeOutputingVariables2

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson03/ChallengeOutputingVariables2.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson03;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-outputing-variables-2/description">LINK</a>
+ */
+public class ChallengeOutputingVariables2 {
+
+  public static void main(String[] args) {
+    int a = 3;
+    char b = 'C';
+    System.out.print(b + "!.....!" + a);
+  }
+}
