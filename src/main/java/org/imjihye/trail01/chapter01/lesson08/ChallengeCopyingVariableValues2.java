835201d5440596acd0d6e5e1e055749f007ddb76
임지혜 <bakcy9842@naver.com>
2025-06-29 18:58:32 +0900

    IntroCopyingVariableValues2

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson08/ChallengeCopyingVariableValues2.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson08;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-copying-variable-values-2/description">LINK</a>
+ */
+public class ChallengeCopyingVariableValues2 {
+
+  public static void main(String[] args) {
+    int a = 5, b = 6, c = 7;
+    a = b = c;
+    System.out.print(a + " " + b + " " + c);
+  }
+}
