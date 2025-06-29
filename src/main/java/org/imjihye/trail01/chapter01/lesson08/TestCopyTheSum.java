c50c265c77416caa251344162491fb4b074df126
임지혜 <bakcy9842@naver.com>
2025-06-29 19:00:25 +0900

    TestCopyTheSum

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson08/TestCopyTheSum.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson08;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-copy-the-sum/description">LINK</a>
+ */
+public class TestCopyTheSum {
+
+  public static void main(String[] args) {
+    int a = 1, b = 2, c = 3;
+    a = b = c = (a+b+c);
+    System.out.print(a + " " + b + " " + c);
+  }
+}
