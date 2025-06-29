0f2a583987704f6e9a31d40b81b6ef9e3a496d51
임지혜 <bakcy9842@naver.com>
2025-06-29 15:54:35 +0900

    TestPrintInVariety

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/TestPrintInVariety.java
@@ -0,0 +1,14 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-print-in-variety/description">LINK</a>
+ */
+public class TestPrintInVariety {
+
+  public static void main(String[] args) {
+    System.out.println("Total days in Year");
+    System.out.println(365);
+    System.out.println("Circumference rate");
+    System.out.println(3.1415926535);
+  }
+}
