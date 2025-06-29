cfb78c46047159f69b3557eef45b0cb1f996b379
임지혜 <bakcy9842@naver.com>
2025-06-29 16:00:31 +0900

    IntroDefineNumbersAndSubstract

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson02/IntroDefineNumbersAndSubstract.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson02;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-define-numbers-and-substract/description">LINK</a>
+ */
+public class IntroDefineNumbersAndSubstract {
+
+  public static void main(String[] args) {
+
+    int a = 97, b = 13;
+    System.out.print(a + " - " + b + " = " + (a-b));
+  }
+}
