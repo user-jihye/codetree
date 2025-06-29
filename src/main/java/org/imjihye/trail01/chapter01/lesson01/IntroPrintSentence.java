66c14f775097522c933fed6bc65d046845f2f35b
임지혜 <bakcy9842@naver.com>
2025-06-29 13:38:06 +0900

    IntroPrintSentence

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/IntroPrintSentence.java
@@ -0,0 +1,11 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-sentence/description">LINK</a>
+ */
+public class IntroPrintSentence {
+
+  public static void main(String[] args) {
+    System.out.print("Hello World!");
+  }
+}
