7011c8208dab572faab195d2cc877417d94202dd
임지혜 <bakcy9842@naver.com>
2025-06-29 18:18:10 +0900

    IntroReplacingVariableValues

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson04/TestTheProductOfTwoRealNumbers.java
@@ -0,0 +1,12 @@
+package org.imjihye.trail01.chapter01.lesson04;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-the-product-of-two-real-numbers/description">LINK</a>
+ */
+public class TestTheProductOfTwoRealNumbers {
+
+  public static void main(String[] args) {
+    double a = 5.26, b = 8.27;
+    System.out.printf("%.3f", a * b);
+  }
+}
---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson05/IntroReplacingVariableValues.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson05;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-replacing-variable-values/description">LINK</a>
+ */
+public class IntroReplacingVariableValues {
+
+  public static void main(String[] args) {
+    int a = 3;
+    a = 6;
+    System.out.print(a);
+  }
+}
