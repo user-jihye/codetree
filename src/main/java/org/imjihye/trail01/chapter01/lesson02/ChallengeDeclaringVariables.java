6adc89937940af91df9d8deae0b9ec9c751b8974
임지혜 <bakcy9842@naver.com>
2025-06-29 16:02:38 +0900

    ChanllengeDeclaringVariables

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson02/ChallengeDeclaringVariables.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter01.lesson02;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-declaring-variables/description">LINK</a>
+ */
+public class ChallengeDeclaringVariables {
+
+  public static void main(String[] args) {
+
+    int a = 3;
+    char b = 'C';
+
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
