c7d51879e08e135fd6db0b18feaa89026687ef4d
임지혜 <bakcy9842@naver.com>
2025-06-29 13:36:29 +0900

    IntroPrintWord

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/IntroPrintWord.java
@@ -0,0 +1,11 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-word/description">LINK</a>
+ */
+public class IntroPrintWord {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
