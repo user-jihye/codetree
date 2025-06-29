11e314eb5b187666dbdaf2be4a20c4a131294105
임지혜 <bakcy9842@naver.com>
2025-06-29 15:52:27 +0900

    ChallengePrintTwoSentencesIntroduce

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/ChallengePrintTwoSentencesIntroduce.java
@@ -0,0 +1,12 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-print-two-sentences-introduce/description">LINK</a>
+ */
+public class ChallengePrintTwoSentencesIntroduce {
+
+  public static void main(String[] args) {
+    System.out.println("Hello students!");
+    System.out.println("Welcome to LeebrosCode!");
+  }
+}
