eec464a3179f621d2f88d73b802513230e29207e
임지혜 <bakcy9842@naver.com>
2025-06-29 15:51:06 +0900

    ChallengePrintOneLine

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/ChallengePrintOneLine.java
@@ -0,0 +1,11 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-print-one-line/description">LINK</a>
+ */
+public class ChallengePrintOneLine {
+
+  public static void main(String[] args) {
+    System.out.print("Let's go LeebrosCode!");
+  }
+}
---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson01/IntroPrintTwoLines.java
@@ -0,0 +1,12 @@
+package org.imjihye.trail01.chapter01.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-two-lines/description">LINK</a>
+ */
+public class IntroPrintTwoLines {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    System.out.println("World");
+  }
+}
