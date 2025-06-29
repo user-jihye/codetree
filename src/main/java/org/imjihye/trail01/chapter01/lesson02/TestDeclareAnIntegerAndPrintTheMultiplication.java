98e899a14dc14f0906484174f21607c2de12dd07
임지혜 <bakcy9842@naver.com>
2025-06-29 16:15:29 +0900

    ChallengeOutputingVariables

---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson02/TestDeclareAnIntegerAndPrintTheMultiplication.java
@@ -0,0 +1,13 @@
+package org.imjihye.trail01.chapter01.lesson02;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-Declare-an-integer-and-print-the-multiplication/description">LINK</a>
+ */
+public class TestDeclareAnIntegerAndPrintTheMultiplication {
+
+  public static void main(String[] args) {
+
+    int a = 26, b = 5;
+    System.out.println(a + " * " + b + " = " + (a*b));
+  }
+}
---
+++ src/main/java/org/imjihye/trail01/chapter01/lesson03/ChallengeOutputingVariables.java
@@ -0,0 +1,14 @@
+package org.imjihye.trail01.chapter01.lesson03;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-outputing-variables/description">LINK</a>
+ */
+public class ChallengeOutputingVariables {
+
+  public static void main(String[] args) {
+
+    int a = 3;
+    char b = 'C';
+    System.out.printf("%d...%c", a, b);
+  }
+}
