0136b7c1bb49de1f5716989774c7e783e607b0e7
임지혜 <bakcy9842@naver.com>
2025-07-02 01:36:42 +0900

    ChallengeInputAndOutput

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson04/ChallengeInputAndOutput.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson04;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-input-and-output/description">LINK</a>
+ */
+public class ChallengeInputAndOutput {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.print(a + " " + b);
+  }
+}
