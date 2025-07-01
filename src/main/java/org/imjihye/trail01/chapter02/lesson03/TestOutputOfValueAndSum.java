3056e05c8dd776d23dca6f82550ebce4600d8cdc
임지혜 <bakcy9842@naver.com>
2025-07-01 19:20:28 +0900

    TestOutputOfValueAndSum

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson03/TestOutputOfValueAndSum.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson03;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-output-of-value-and-sum/description">LINK</a>
+ */
+public class TestOutputOfValueAndSum {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.printf("%d %d %d", a, b, (a+b));
+  }
+}
