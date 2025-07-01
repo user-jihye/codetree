eb524a41844c04c4869ba75ee22605236de36055
임지혜 <bakcy9842@naver.com>
2025-07-01 19:17:40 +0900

    ChallengeChangingInputs

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson03/ChallengeChangingInputs.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson03;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-changing-inputs/description">LINK</a>
+ */
+public class ChallengeChangingInputs {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.print(b + " " + a);
+  }
+}
