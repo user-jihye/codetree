8e532008a24f08f28a6526260104ff02821cd604
임지혜 <bakcy9842@naver.com>
2025-07-02 02:08:21 +0900

    IntroEnterWordAndPrint

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson05/IntroEnterWordAndPrint.java
@@ -0,0 +1,15 @@
+package org.imjihye.trail01.chapter02.lesson05;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-enter-word-and-print/description">LINK</a>
+ */
+public class IntroEnterWordAndPrint {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    System.out.print(s);
+  }
+}
