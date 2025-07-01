0c3569807a663d186f5034df3963c6ee82521fe1
임지혜 <bakcy9842@naver.com>
2025-07-01 19:02:12 +0900

    TestCalculationByInputtingAnInteger

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson01/TestCalculationByInputtingAnInteger.java
@@ -0,0 +1,17 @@
+package org.imjihye.trail01.chapter02.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-calculation-by-inputting-an-integer/description">LINK</a>
+ */
+public class TestCalculationByInputtingAnInteger {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+
+    System.out.print(a * 2 + 3);
+  }
+}
