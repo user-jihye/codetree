34f59a82bf45df12d758d53b1dc9250e61a07247
임지혜 <bakcy9842@naver.com>
2025-07-02 01:35:06 +0900

    IntroInputCalculate3

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson04/IntroInputCalculate3.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson04;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-input-calculate-3/description">LINK</a>
+ */
+public class IntroInputCalculate3 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.print(a * b);
+  }
+}
