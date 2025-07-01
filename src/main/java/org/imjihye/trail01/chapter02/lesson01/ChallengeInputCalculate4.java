b588b0bc00f1af75662f358886eeb66c8ba3235f
임지혜 <bakcy9842@naver.com>
2025-07-01 19:00:44 +0900

    ChallengeInputCalculate4

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson01/ChallengeInputCalculate4.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-input-calculate-4/description">LINK</a>
+ */
+public class ChallengeInputCalculate4 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.print(a * 2);
+  }
+}
