d078f12290fa08af322538e6ed9fb698e5234ca0
임지혜 <bakcy9842@naver.com>
2025-07-01 18:58:15 +0900

    ChallengeEnterIntAndPrintScore

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson01/ChallengeEnterIntAndPrintScore.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-enter-int-and-print-score/description">LINK</a>
+ */
+public class ChallengeEnterIntAndPrintScore {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.printf("Your score is %d point.", n);
+  }
+}
