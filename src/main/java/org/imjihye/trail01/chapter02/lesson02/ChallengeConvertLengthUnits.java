e87007b7e35ad2c73a8fdac470fac30444f91275
임지혜 <bakcy9842@naver.com>
2025-07-01 19:08:41 +0900

    ChallengeConvertLengthUnits

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson02/ChallengeConvertLengthUnits.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson02;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-convert-length-units/description">LINK</a>
+ */
+public class ChallengeConvertLengthUnits {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    double n = sc.nextDouble();
+    System.out.printf("%.1f", n * 30.48);
+  }
+}
