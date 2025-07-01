e1edfd818f814f9b0929a08fb79383705f2cae45
임지혜 <bakcy9842@naver.com>
2025-07-01 19:10:16 +0900

    TestCalculationByInputtingARealNumber2

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson02/TestCalculationByInputtingARealNumber2.java
@@ -0,0 +1,15 @@
+package org.imjihye.trail01.chapter02.lesson02;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-calculation-by-inputting-a-real-number-2/description">LINK</a>
+ */
+public class TestCalculationByInputtingARealNumber2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double a = sc.nextDouble();
+    System.out.printf("%.2f", a + 1.5);
+  }
+}
