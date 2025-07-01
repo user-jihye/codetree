23f1fb048bab3c084565ef3cb9ed4ed2719b353b
임지혜 <bakcy9842@naver.com>
2025-07-02 02:12:59 +0900

    TestChangeOrderOfStrings

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson05/TestChangeOrderOfStrings.java
@@ -0,0 +1,18 @@
+package org.imjihye.trail01.chapter02.lesson05;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-change-order-of-strings/description">LINK</a>
+ */
+public class TestChangeOrderOfStrings {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String S = sc.next();
+    String T = sc.next();
+
+    System.out.println(T);
+    System.out.println(S);
+  }
+}
