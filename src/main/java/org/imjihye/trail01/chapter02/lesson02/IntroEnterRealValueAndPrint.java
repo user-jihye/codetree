c49378d9b0f0e9a7a170f125dfaff2430f96cad5
임지혜 <bakcy9842@naver.com>
2025-07-01 19:05:08 +0900

    IntroEnterRealValueAndPrint

---
+++ src/main/java/org/imjihye/trail01/chapter02/lesson02/IntroEnterRealValueAndPrint.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter02.lesson02;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-enter-real-value-and-print/description">LINK</a>
+ */
+public class IntroEnterRealValueAndPrint {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    double n = sc.nextDouble();
+    System.out.printf("%.2f", n);
+  }
+}
