72eba926a4f4814e12d68cfe8c6d6e3e244c928c
임지혜 <bakcy9842@naver.com>
2025-07-03 21:03:32 +0900

    IntroSumAndAvg

---
+++ src/main/java/org/imjihye/trail01/chapter03/lesson02/IntroSumAndAvg.java
@@ -0,0 +1,21 @@
+package org.imjihye.trail01.chapter03.lesson02;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-sum-and-avg/description">LINK</a>
+ */
+public class IntroSumAndAvg {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int sum = a + b;
+    double avg = (double)sum / 2;
+
+    System.out.print(sum + " " + avg);
+  }
+}
