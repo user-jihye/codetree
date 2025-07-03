713f23b79f3201506c259fc9bcbb556a5fcb9d97
임지혜 <bakcy9842@naver.com>
2025-07-03 20:42:13 +0900

    ChallengeMultipleAndQuotientOfTwoNumbers

---
+++ src/main/java/org/imjihye/trail01/chapter03/lesson01/ChallengeMultipleAndQuotientOfTwoNumbers.java
@@ -0,0 +1,20 @@
+package org.imjihye.trail01.chapter03.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-multiple-and-quotient-of-two-numbers/description">LINK</a>
+ */
+public class ChallengeMultipleAndQuotientOfTwoNumbers {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.printf("%d * %d = %d\n", a, b, a*b);
+    System.out.printf("%d / %d = %d", a, b, a/b);
+  }
+}
