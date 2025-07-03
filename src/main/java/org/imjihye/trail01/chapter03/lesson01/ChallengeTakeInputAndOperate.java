e5869401280f89db939ffadf23b0bcec5cd3b408
임지혜 <bakcy9842@naver.com>
2025-07-03 20:53:00 +0900

    ChallengeTakeInputAndOperate

---
+++ src/main/java/org/imjihye/trail01/chapter03/lesson01/ChallengeTakeInputAndOperate.java
@@ -0,0 +1,19 @@
+package org.imjihye.trail01.chapter03.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-take-input-and-operate/description">LINK</a>
+ */
+public class ChallengeTakeInputAndOperate {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + 87);
+    System.out.println(b % 10);
+  }
+}
