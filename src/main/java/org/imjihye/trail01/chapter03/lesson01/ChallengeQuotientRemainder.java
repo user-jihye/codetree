bf48ddf9310f9e285ff75c2e38af3775a5517c09
임지혜 <bakcy9842@naver.com>
2025-07-03 20:50:39 +0900

    ChallengeQuotientRemainder

---
+++ src/main/java/org/imjihye/trail01/chapter03/lesson01/ChallengeQuotientRemainder.java
@@ -0,0 +1,18 @@
+package org.imjihye.trail01.chapter03.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-quotient-remainder/description">LINK</a>
+ */
+public class ChallengeQuotientRemainder {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.print((a / b) + "..." + (a % b));
+  }
+}
