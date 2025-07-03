d169015ae80b62d9fea041fc4e6c6ae81a09b2e7
임지혜 <bakcy9842@naver.com>
2025-07-03 20:34:54 +0900

    IntroSimpleArithmeticOperation

---
+++ src/main/java/org/imjihye/trail01/chapter03/lesson01/IntroSimpleArithmeticOperation.java
@@ -0,0 +1,21 @@
+package org.imjihye.trail01.chapter03.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-simple-arithmetic-operation/description">LINK</a>
+ */
+public class IntroSimpleArithmeticOperation {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+  }
+}
