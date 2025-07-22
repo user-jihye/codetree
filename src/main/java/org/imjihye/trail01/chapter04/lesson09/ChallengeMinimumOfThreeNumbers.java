00db0f2081b840ba51938c68baab79f6930eb458
임지혜 <bakcy9842@naver.com>
2025-07-22 20:44:55 +0900

    ChallengeMinimumOfThreeNumbers

---
+++ src/main/java/org/imjihye/trail01/chapter04/lesson09/ChallengeMinimumOfThreeNumbers.java
@@ -0,0 +1,26 @@
+package org.imjihye.trail01.chapter04.lesson09;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-minimum-of-three-numbers/description">LINK</a>
+ */
+public class ChallengeMinimumOfThreeNumbers {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a, b, c;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c = sc.nextInt();
+
+    if (a <= b && a <= c) {
+      System.out.println(a);
+    } else if (b <= a && b <= c) {
+      System.out.println(b);
+    } else {
+      System.out.println(c);
+    }
+  }
+}
