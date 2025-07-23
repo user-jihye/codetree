17cd7944a2557e9f8625c8b630c029fc3797a95d
임지혜 <bakcy9842@naver.com>
2025-07-23 21:23:14 +0900

    ChallengePrintString

---
+++ src/main/java/org/imjihye/trail01/chapter05/lesson07/ChallengePrintString.java
@@ -0,0 +1,19 @@
+package org.imjihye.trail01.chapter05.lesson07;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-print-string/description">LINK</a>
+ */
+public class ChallengePrintString {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (int i = 0; i < n; i++) {
+      System.out.println("LeebrosCode");
+    }
+  }
+}
