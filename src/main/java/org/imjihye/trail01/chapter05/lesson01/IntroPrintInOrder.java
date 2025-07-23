d983f0e461fc2a547b83ce20db291bfff68dec3f
임지혜 <bakcy9842@naver.com>
2025-07-23 13:47:15 +0900

    IntroPrintInOrder

---
+++ src/main/java/org/imjihye/trail01/chapter05/lesson01/IntroPrintInOrder.java
@@ -0,0 +1,14 @@
+package org.imjihye.trail01.chapter05.lesson01;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-in-order/description">LINK</a>
+ */
+public class IntroPrintInOrder {
+
+  public static void main(String[] args) {
+
+    for (int i = 5; i <= 17; i++) {
+      System.out.print(i + " ");
+    }
+  }
+}
