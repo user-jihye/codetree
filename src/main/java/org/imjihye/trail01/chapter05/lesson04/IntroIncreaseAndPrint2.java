4334eaaf7e33a945c9a610f79cbf79fd319b9707
임지혜 <bakcy9842@naver.com>
2025-07-23 14:43:35 +0900

    IntroIncreaseAndPrint2

---
+++ src/main/java/org/imjihye/trail01/chapter05/lesson04/IntroIncreaseAndPrint2.java
@@ -0,0 +1,16 @@
+package org.imjihye.trail01.chapter05.lesson04;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-increase-and-print-2/description">LINK</a>
+ */
+public class IntroIncreaseAndPrint2 {
+
+  public static void main(String[] args) {
+
+    int x = 10;
+    while (x <= 26) {
+      System.out.print(x + " ");
+      x++;
+    }
+  }
+}
