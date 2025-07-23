16f99b10e0bde239b8c1988a54211dcaad358c3d
임지혜 <bakcy9842@naver.com>
2025-07-23 14:19:22 +0900

    IntroDecreaseAndPrint

---
+++ src/main/java/org/imjihye/trail01/chapter05/lesson03/IntroDecreaseAndPrint.java
@@ -0,0 +1,14 @@
+package org.imjihye.trail01.chapter05.lesson03;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-decrease-and-print/description">LINK</a>
+ */
+public class IntroDecreaseAndPrint {
+
+  public static void main(String[] args) {
+
+    for (int i = 17; i >= 5; i--) {
+      System.out.print(i + " ");
+    }
+  }
+}
