1b19a027145c8433f794af83801be90b28bda4fe
임지혜 <bakcy9842@naver.com>
2025-08-02 16:58:56 +0900

    TestOutputDefferentlyEachRow

---
+++ src/main/java/org/imjihye/trail01/chapter06/lesson11/TestOutputDefferentlyEachRow.java
@@ -0,0 +1,31 @@
+package org.imjihye.trail01.chapter06.lesson11;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/test-output-defferently-each-row/description">LINK</a>
+ */
+public class TestOutputDefferentlyEachRow {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int cnt = 0;
+
+    for (int y = 1; y <= n; y++) {
+      if (y % 2 == 1) {
+        for (int x = 1; x <= n; x++) {
+          cnt += 1;
+          System.out.print(cnt + " ");
+        }
+      } else {
+        for (int x = 1; x <= n; x++) {
+          cnt += 2;
+          System.out.print(cnt + " ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
