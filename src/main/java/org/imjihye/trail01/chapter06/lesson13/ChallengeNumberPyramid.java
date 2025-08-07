a1667f00462ec08480570b799c575328e3d977c5
임지혜 <bakcy9842@naver.com>
2025-08-07 15:55:12 +0900

    ChallengeNumberPyramid

---
+++ src/main/java/org/imjihye/trail01/chapter06/lesson13/ChallengeNumberPyramid.java
@@ -0,0 +1,21 @@
+package org.imjihye.trail01.chapter06.lesson13;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-number-pyramid/description">LINK</a>
+ */
+public class ChallengeNumberPyramid {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
+  }
+}
