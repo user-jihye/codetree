9df987fd06a6c8846de67e65e1a6e0dc7c12020f
임지혜 <bakcy9842@naver.com>
2025-08-14 02:54:02 +0900

    ChallengeReceive10Inputs

---
+++ src/main/java/org/imjihye/trail01/chapter07/lesson01/ChallengeReceive10Inputs.java
@@ -0,0 +1,33 @@
+package org.imjihye.trail01.chapter07.lesson01;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-receive-10-inputs/description">LINK</a>
+ */
+public class ChallengeReceive10Inputs {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int[] arr = new int[10];
+    int cnt = 0;
+
+    for (int i = 0; i < 10; i++) {
+      arr[i] = sc.nextInt();
+
+      if (arr[i] == 0)
+        break;
+
+      cnt++;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < cnt; i++) {
+      sum += arr[i];
+    }
+
+    double avg = (double) sum / cnt;
+    System.out.printf("%d %.1f", sum, avg);
+  }
+}
