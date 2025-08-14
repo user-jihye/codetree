6740fc040ae929225fa0dd356dd919ac70177e9b
임지혜 <bakcy9842@naver.com>
2025-08-14 15:34:41 +0900

    ChallengeReach100

--- src/main/java/org/imjihye/trail01/chapter07/lesson03/ChallengeReach100.java
+++ src/main/java/org/imjihye/trail01/chapter07/lesson03/ChallengeReach100.java
@@ -1,11 +1,32 @@
 package org.imjihye.trail01.chapter07.lesson03;
 
+import java.util.Scanner;
+
 /**
  * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-reach-100/description">LINK</a>
  */
 public class ChallengeReach100 {
 
   public static void main(String[] args) {
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    int[] arr = new int[100];
+    arr[0] = 1;
+    arr[1] = sc.nextInt();
+    int point100 = 0;
+
+    for (int i = 2; i < 100; i++) {
+      arr[i] =  arr[i-2] + arr[i-1];
+
+      if (arr[i] > 100) {
+        point100 = i;
+        break;
+      }
+    }
+
+    for (int i = 0; i <= point100; i++) {
+      System.out.print(arr[i] + " ");
+    }
   }
 }
