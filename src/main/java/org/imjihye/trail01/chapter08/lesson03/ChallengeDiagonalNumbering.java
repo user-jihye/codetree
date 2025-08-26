d737e135a9e5dd46da28dca325eb419f55eca7d8
임지혜 <bakcy9842@naver.com>
2025-08-27 02:47:08 +0900

    ChallengeDiagonalNumbering

--- src/main/java/org/imjihye/trail01/chapter08/lesson03/ChallengeDiagonalNumbering.java
+++ src/main/java/org/imjihye/trail01/chapter08/lesson03/ChallengeDiagonalNumbering.java
@@ -14,7 +14,38 @@ public class ChallengeDiagonalNumbering {
     n = sc.nextInt();
     m = sc.nextInt();
 
+    int col, row;
     int[][] arr = new int[n][m];
+    for (int x = 0; x < m; x++) {
+      if (x == m - 1) {
+        for (int y = 0; y < n; y++) {
+          row = y;
+          col = x;
+          while (row < n && col >= 0) {
+            arr[row][col] = num;
+            row++;
+            col--;
+            num++;
+          }
+        }
 
+      } else {
+        row = 0;
+        col = x;
+        while (row < n && col >= 0) {
+          arr[row][col] = num;
+          row++;
+          col--;
+          num++;
+        }
+      }
+    }
+
+    for (int y = 0; y < n; y++) {
+      for (int x = 0; x < m; x++) {
+        System.out.print(arr[y][x] + " ");
+      }
+      System.out.println();
+    }
   }
 }
