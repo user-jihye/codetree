c41fcdc31fd46c19b7e7e5232cc05fce24ea1504
임지혜 <bakcy9842@naver.com>
2025-08-29 01:46:03 +0900

    IntroPrintStringTwice

---
+++ src/main/java/org/imjihye/trail01/chapter09/lesson01/IntroPrintStringTwice.java
@@ -0,0 +1,18 @@
+package org.imjihye.trail01.chapter09.lesson01;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-string-twice/description">LINK</a>
+ */
+public class IntroPrintStringTwice {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    for (int i = 0; i < 2; i++)
+      System.out.println(str);
+  }
+}
