12bc21446a6ce2a8234662192a2058942721486d
임지혜 <bakcy9842@naver.com>
2025-09-02 22:56:32 +0900

    ChallengeSeparateWordsWithWords

---
+++ src/main/java/org/imjihye/trail01/chapter09/lesson03/ChallengeSeparateWordsWithWords2.java
@@ -0,0 +1,11 @@
+package org.imjihye.trail01.chapter09.lesson03;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-separate-words-with-words-2/description">LINK</a>
+ */
+public class ChallengeSeparateWordsWithWords2 {
+
+  public static void main(String[] args) {
+    
+  }
+}
