5871a46cce7b9db121b8793b8608abc2de58443a
임지혜 <bakcy9842@naver.com>
2025-09-02 23:04:35 +0900

    ChallengeStringEndingWithSpecificCharacter

---
+++ src/main/java/org/imjihye/trail01/chapter09/lesson03/ChallengeStringEndingWithSpecificCharacter.java
@@ -0,0 +1,30 @@
+package org.imjihye.trail01.chapter09.lesson03;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-string-ending-with-specific-character/description">LINK</a>
+ */
+public class ChallengeStringEndingWithSpecificCharacter {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String[] str = new String[10];
+
+    for (int i = 0; i < 10; i++)
+      str[i] = sc.next();
+
+    char x = sc.next().charAt(0);
+    boolean isExist = false;
+    for (int i = 0; i < 10; i++) {
+      if (str[i].charAt(str[i].length() - 1) == x) {
+        System.out.println(str[i]);
+        isExist = true;
+      }
+    }
+
+    if (!isExist)
+      System.out.println("None");
+  }
+}
