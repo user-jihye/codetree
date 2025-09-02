be8b9684a3ee13358628a20d5ef22e8aa54bd829
임지혜 <bakcy9842@naver.com>
2025-09-02 22:54:24 +0900

    ChallengeSeparateWordsWithWords

---
+++ src/main/java/org/imjihye/trail01/chapter09/lesson03/ChallengeSeparateWordsWithWords.java
@@ -0,0 +1,24 @@
+package org.imjihye.trail01.chapter09.lesson03;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-separate-words-with-words/description">LINK</a>
+ */
+public class ChallengeSeparateWordsWithWords {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String[] str = new String[10];
+
+    for (int i = 0; i < 10; i++) {
+      str[i] = sc.next();
+    }
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println(str[i]);
+    }
+  }
+}
