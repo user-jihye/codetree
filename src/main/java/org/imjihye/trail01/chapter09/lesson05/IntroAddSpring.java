310ef4e3e9e3ca6ababf487fc42ed311775b8faa
임지혜 <bakcy9842@naver.com>
2025-09-10 09:58:36 +0900

    IntroAddSpring

---
+++ src/main/java/org/imjihye/trail01/chapter09/lesson05/IntroAddSpring.java
@@ -0,0 +1,18 @@
+package org.imjihye.trail01.chapter09.lesson05;
+
+import java.util.Scanner;
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-add-spring/description">LINK</a>
+ */
+public class IntroAddSpring {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    System.out.println(str + "Hello");
+
+  }
+}
